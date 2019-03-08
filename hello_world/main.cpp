ddd4a5df084530ead7ffbb53504def70909618b6
Sven Stroteich <ss133050@uni-greifswald.de>
2019-03-08 11:05:50 +0100

    added function multiply

--- hello_world/main.cpp
+++ hello_world/main.cpp
@@ -4,7 +4,12 @@ void print_hello_world(){
   std::cout << "Hello World" << std::endl;
   } 
 
+int multiply(double a, double b){
+  return a*b;
+}
+
 int main(){
     print_hello_world();
+    std::cout << multiply(3,2) << std::endl;
 }
 	
