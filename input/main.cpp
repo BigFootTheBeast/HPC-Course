92ac86be77da55e48bf0e496f3e103bdb766f123
Sven Stroteich <ss133050@uni-greifswald.de>
2019-03-08 14:11:31 +0100

    added containers

--- input/main.cpp
+++ input/main.cpp
@@ -1,23 +1,48 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <vector>
 
 void analysis(){
   std::cout << "implement the super complicated analysis code here" << std::endl;
 }
 
 int main(int argc, char** argv){
-  std::ifstream in("run_analysis.txt");
-  bool end = in.eof();
-  while (end == false){
-      end = in.eof();
-      std::cout << "end = " <<  end << std::endl;
+/*  std::ifstream in("run_analysis.txt");
+  while (1){
       double number1 = 0, number2 = 0;
-      in >> number1;
-      in >> number2;
+      if( !(in>>number1) ) break;
+      if( !(in>>number2) ) break;
       double result = number1 * number2;
       std::cout << "Result = " << result << std::endl;
-      end = in.eof();
+  }
+*/
+  std::ifstream in2("run_analysis.txt");
+  std::vector<std::vector<double>> in_cont;
+  while (true){
+    std::string line;
+    if( !(std::getline(in2,line)) ) break;
+    std::stringstream ss(line) ;
+    double number1 = 0, number2 = 0;
+    ss>>number1;
+    ss>>number2;
+    std::vector<double> numbers;
+    numbers.push_back(number1);
+    numbers.push_back(number2); 
+
+    in_cont.push_back(numbers);
+
   }
 
+  std::vector<double> out_cont;
+  for( int i = 0; i < in_cont.size(); i ++){
+    double result = in_cont[i][0] * in_cont[i][1];
+    out_cont.push_back(result);
+  }
+
+  std::ofstream out("results.txt");
+  for( double& e : out_cont){
+    out << e << std::endl;
+  }
     return 0;
 }
