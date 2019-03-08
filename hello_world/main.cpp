2bf9bf023eca8288a266ac5aa5cf74792f3ed3e0
Sven Stroteich <ss133050@uni-greifswald.de>
2019-03-08 12:21:13 +0100

    added meow

--- hello_world/main.cpp
+++ hello_world/main.cpp
@@ -9,11 +9,21 @@ double multiply(double a, double b){
 }
 
 int main(){
-    double a=0, b=0;
+/*    double a=0, b=0;
     std::cout << "Write your numbers" << std::endl;
     std::cin >> a >> b;
-
-    print_hello_world();
-    std::cout << multiply(a,b) << std::endl;
+    */
+    std::cout << "hello world or meow? h|m" << std::endl;
+    char answer;
+    std::cin >> answer;
+    if ( answer == 'h'){
+      print_hello_world();
+    }else if ( answer == 'm'){
+      std::cout << "meow" << std::endl;
+    }else{
+      std::cout << "h and m are valid options" << std::endl;
+    }
+    return 0;
+    //std::cout << multiply(a,b) << std::endl;
 }
 	
